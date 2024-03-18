5fca36397fe8845b6f048112e984864a478cf05c
JaveedYara72 <javeedymd.iiitg@gmail.com>
2024-03-18 18:00:23 +0530

    made the basic html template and started doing it in django

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  email = models.CharField(max_length=255)
+  phone_number = models.CharField(max_length=12)
+  address = models.CharField(max_length=255)
+
+class PizzaBase(models.Model):
+    name = models.CharField(max_length=100)
+
+class Cheese(models.Model):
+    name = models.CharField(max_length=100)
+
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
--- customers/views.py
+++ customers/views.py
@@ -2,5 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 def customers(request):
+  # Variables in Django, youll pass it like this and parse it as pizzaname in general in html
+  var = {'pizzaname': 'Mozzarella'}
   template = loader.get_template('index.html')
-  return HttpResponse(template.render())
+  return HttpResponse(template.render(var, request))
